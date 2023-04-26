6ce80ab60e2ecd4ab80c209b33e8f285590e6a06
VladHolovin27 <105449427+VladHolovin27@users.noreply.github.com>
2023-04-26 14:38:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+ let user = {
+ height: 20,
+ width: 30
+ };
+function userResult() {
+    let result = height + width;
+    console.log(result);
+}
+
+
+function jgo(callback1, callback, number)
+   if (number > 10) {
+    callback1();
+   else {
+    callback();
+    }
+}
+ jgo((callback1) => {Vlad}, (callback) => {Artem});
